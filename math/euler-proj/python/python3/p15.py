7575972df772b30e1a2c1e540c709c35b65ba610
Ian S Pringle <ipringle@protonmail.com>
2018-12-01 21:04:51 -0500

    Project 15 answers, thx binomial coefficient!

---
+++ math/euler-proj/python/python3/p15.py
@@ -0,0 +1,15 @@
+"""
+For a square grid of size n, how many possible ways can you go from (0,n) to (n,0)
+if you can only travel down and to the right?
+Answer for a grid of size 20: 137846528820
+"""
+
+gridSize = 20
+paths = 1
+
+i = 0
+while i < gridSize:
+	paths *= (2 * gridSize) - i
+	paths /= i + 1
+	i += 1
+print(paths)
