6affa4db669036651907a3f81a1eab1f0d66fd64
Ian S Pringle <ipringle@protonmail.com>
2018-12-02 22:11:32 -0500

    p17.py added. LULZ

---
+++ math/euler-proj/python/python3/p17.py
@@ -0,0 +1,11 @@
+sometimes_programs_arent_needed = '''Sometimes writing a program isn't the easiest way:
+	ones:    3 + 3 + 5 + 4 + 4 + 3 + 5 + 5 + 4 =                36
+	teens:   3 + 6 + 6 + 8 + 8 + 7 + 7 + 9 + 8 + 8 =            70
+	20-99:   10 * (6 + 6 + 5 + 5 + 5 + 7 + 6 + 6) + 8 * 36 =   748
+	100-999: (36 * 100) + (9 * 854) + (7 * 9) + (999 * 10) = 20259
+	1000:                                                       11
+	--------------------------------------------------------------
+	TOTAL                                                    21124
+	'''
+
+print(sometimes_programs_arent_needed)
