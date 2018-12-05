026a70d6f55f9bdab8c7dd19ba23ed12c73dd55f
Ian S. Pringle <ipringle@protonmail.com>
2018-12-05 05:45:58 -0500

    O P T I M I Z E D

--- math/euler-proj/python/python3/p21.py
+++ math/euler-proj/python/python3/p21.py
@@ -1,16 +1,29 @@
 def d(n):
-	divs = []
-	for i in range(1,n):
-		if n % i == 0:
-			divs.append(i)
-	return divs
+    divs = []
+    if n == 1: return [0]
+    x = int(n**.5)
+    if x**2 == n:
+       divs.append(x)
+       x -= 1
+    else:
+        divs.append(1)
+    if n % 2 != 0:
+        i = 3
+        s = 2
+    else:
+        i = 2
+        s = 1
+    while i <= x:
+        if n % i == 0:
+            divs.append(i)
+            divs.append(int(n/i))
+        i += s
+    return divs
 
-sums = {}
+sums = 0
 for n in range(1,10001):
-	sums[n] = sum(d(n))
-pairs = []
-for k, v in sums.items():
-	for i in sums.keys():
-		if v == i and sums[i] == k and k != v:
-			pairs.append(k)
-print(sum(pairs))
+    x = sum(d(n))
+    if x > n:
+        if sum(d(x)) == n:
+            sums += n + x
+print(sums)
