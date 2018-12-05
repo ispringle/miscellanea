4c7755c177ae30a37279cbf99e78f2ecc9b96716
Ian S Pringle <ipringle@protonmail.com>
2018-12-05 01:06:07 -0500

    Day 05 Completed!

---
+++ 2018/5/solution.py
@@ -0,0 +1,27 @@
+from input import polymer
+import string
+
+def react(polymer):
+	unit = 0
+	while unit < (len(polymer) - 1):
+		if polymer[unit] != polymer[unit+1] and polymer[unit].lower() == polymer[unit+1].lower():
+			polymer = polymer[:unit]+polymer[unit+2:]
+			unit = max(0, unit-1)
+		else:
+			unit += 1
+	return polymer
+
+def partOne(polymer):
+	return len(react(polymer))
+
+def partTwo(polyer):
+	optimized = len(polymer)
+	for char in string.ascii_lowercase:
+		test_polymer = polymer.replace(char, "").replace(char.upper(), "")
+		optimized = min(optimized, len(react(test_polymer)))
+	return optimized
+
+print(partOne(polymer))
+print(partTwo(polymer))
+
+	
