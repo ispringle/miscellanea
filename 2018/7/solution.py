2c12ca0dc491d13960c506d40504f6cc24f739a8
Ian S Pringle <ipringle@protonmail.com>
2018-12-09 23:48:24 -0500

    low-key rage quitting on 7-2

--- 2018/7/solution.py
+++ 2018/7/solution.py
@@ -16,11 +16,53 @@ def partOne(instructions):
 	last = set([i[1] for i in instructions if i[1] not in [j[0] for j in instructions]])
 	ordered = ''
 	while len(valid) > 0:
-		print(valid)
 		n = next_(valid)
 		ordered += n
 		instructions = purge(instructions, n)
 		valid = set([i[0] for i in instructions if i[0] not in [j[1] for j in instructions]])
+	ordered += next_(last)
+	return ordered
+
+def partTwo(instructions):
+	alphabet = sorted(list("qwertyuiopasdfghjklzxcvbnm"))
+	elf_count = 5
+	time_mod = 60
+	elves = {i: {'e': None, 'w': False, 'i': ''} for i in range(elf_count)}
+	times = {alpha: sec + 1 + time_mod for sec, alpha in enumerate(alphabet)}
+	instructions = parse_(instructions)
+	valid = set([i[0] for i in instructions if i[0] not in [j[1] for j in instructions]])
+	last = set([i[1] for i in instructions if i[1] not in [j[0] for j in instructions]])
+	ordered = ''
+	time = 0
+	working = True
+	unavailable = []
+	while working:
+		for elf in elves.keys():
+			valid = set([i[0] for i in instructions if i[0] not in [j[1] for j in instructions]])
+			valid = [i for i in valid if i not in unavailable]
+			if elves[elf]['w']:
+				working = True
+				if elves[elf]['e'] == time:
+					elves[elf]['w'] = False
+					ordered += elves[elf]['i']
+					instructions = purge(instructions, n)
+					del unavailable[unavailable.index(elves[elf]['i'])]
+					elves[elf]['i'] = ''
+			elif len(valid) > 0:
+				n = next_(valid)
+				unavailable.append(n)
+				elves[elf]['i'] = n
+				elves[elf]['e'] = time + times[n.lower()]
+				elves[elf]['w'] = True
+		working = False
+		for elf in elves.keys():
+			if elves[elf]['w']:
+				working = True
+		time += 1
+		print(time)
+		print(valid)
+		print(unavailable)
+		print(elves)
 	ordered += next_(last)
 	return ordered
 
@@ -34,5 +76,4 @@ test = [
 		"Step F must be finished before step E can begin.",
 ]
 print(partOne(instructions))
-#print(partOne(test))
-
+print(partTwo(instructions))
