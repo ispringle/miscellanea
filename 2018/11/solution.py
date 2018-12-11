e2953041654f93d5787b365ef3ea55c58ca3d484
Ian S Pringle <ipringle@protonmail.com>
2018-12-11 02:59:09 -0500

    Cleaned up part 1, part 2 is not working at the moment :(

--- 2018/11/solution.py
+++ 2018/11/solution.py
@@ -1,5 +1,6 @@
 def powerLevel(x, y):
-	gsn = 2568
+	#gsn = 2568
+	gsn = 18
 	rackID = x + 10
 	power = rackID * y
 	power += gsn
@@ -7,18 +8,34 @@ def powerLevel(x, y):
 	power = int(power / 100)
 	return power % 10 - 5
 
-def squarePower(x, y):
-	return sum([sum([powerLevel(i, j) for j in range(y, y + 3)]) for i in range(x, x + 3)])
+def squarePower(x, y, s):
+	return sum([sum([powerLevel(i, j) for j in range(y, y + s)]) for i in range(x, x + s)])
 
 def partOne():
-	grid = [[squarePower(x, y) for x in range(1, 299)] for y in range(1, 299)]
+	grid = [[squarePower(x, y, 3) for x in range(1, 299)] for y in range(1, 299)]
+	best = max([max(i) for i in grid])
+	#return [[[x+1,y+1] for x in range(len(grid[y])) if grid[y][x] == best] for y in range(len(grid))]
 	loc = []
-	best = -100
 	for y in range(len(grid)):
 		for x in range(len(grid[y])):
-			if grid[y][x] > best:
-				best = grid[y][x]
+			if grid[y][x] == best:
 				loc = [x + 1, y + 1]
 	return loc
 
+def partTwo():
+	best = -1000000
+	loc = []
+	size = 0
+	for s in range(1,300):
+		grid = [[squarePower(x, y, 3) for x in range(1, 301 - s)] for y in range(1, 301 - s)]
+		for y in range(len(grid)):
+			for x in range(len(grid[y])):
+				if grid[y][x] > best:
+					best = grid[y][x]
+					loc = [x + 1, y + 1]
+					size = s
+	return loc + [size]
+
+
 print(partOne())
+#print(partTwo())
