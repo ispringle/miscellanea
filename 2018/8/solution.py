d7a44212f10387255f4733838f21f2c5b6dd7e8f
Ian S Pringle <ipringle@protonmail.com>
2018-12-11 02:30:17 -0500

    Dau 8 part 1 solved and lookin' pretty

--- 2018/8/solution.py
+++ 2018/8/solution.py
@@ -1,28 +1,22 @@
 from input import tree
-
-def parse_(tree, metadata):
-	if len(tree) > 0:
-		#print(f"Tree: {tree}")
-		nodes = tree[0]
-		meta = tree[1]
-		if tree[0] < 1:
-			#print(f"Metadata: {tree[2:2 + meta]}")
-			metadata.append(tree[2: 2 + meta])
-			parse_(tree[2+meta:], metadata)
-		else: 
-			#print(f"Metadata: {tree[-meta:]}")
-			metadata.append(tree[-meta:])
-			parse_(tree[2:-meta], metadata)
-	return metadata
-
-def partOne(tree):
-	metadata = parse_(tree, [])
-	sum = 0
-	for packet in metadata:
-		for data in packet:
-			sum += data
-	return sum
+from collections.abc import Iterable
 
 test = [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2]
-print(partOne(test))
 
+def parse_(tree):
+	children, metadata = [tree.pop(0) for _ in range(2)]
+	data = []
+	data.extend([parse_(tree) for _ in range(children)])
+	return data + [tree.pop(0) for _ in range(metadata)]
+
+def flatten(lists):
+	for l in lists:
+		if isinstance(l, Iterable) and not isinstance(l, (str, bytes)):
+			yield from flatten(l)
+		else:
+			yield l
+
+def partOne(tree):
+	return sum(flatten(parse_(tree)))
+
+print(partOne(tree))
