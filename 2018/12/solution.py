534a5e80a91e8df57265d28ad6b95b0368be7382
Ian S Pringle <ipringle@protonmail.com>
2018-12-12 02:11:35 -0500

    I do not like poorly worded problems

--- 2018/12/solution.py
+++ 2018/12/solution.py
@@ -1,49 +1,33 @@
-#from input import initial_state, observations
+from input import initial_state, observations
 
-"""Test data"""
-initial_state = '#..#.#..##......###...###'
+def countPlants(pots):
+	d = (len(pots) - 100) //2
+	s = 0
+	for n, pot in enumerate(pots):
+		if pot == '#':
+			s += (n - diff)
+	return s
 
-observations = {
-			'...##' : '#',
-			'..#..' : '#',
-			'.#...' : '#',
-			'.#.#.' : '#',
-			'.#.##' : '#',
-			'.##..' : '#',
-			'.####' : '#',
-			'#.#.#' : '#',
-			'#.###' : '#',
-			'##.#.' : '#',
-			'##.##' : '#',
-			'###..' : '#',
-			'###.#' : '#',
-			'####.' : '#'
-}
+c_gen = initial_state
+p_count = countPlants(initial_state)
+differences = []
+generations = 1000
+gg = 50000000000
 
-pots = "."*30 + initial_state + "."*300
-#initial_state = deque(initial_state.split())
-
-generations = 20
-plants = 0
-next_gen = [pot for pot in pots]
-for gen in range(generations):
-	print(pots)
-	for i in range(len(pots)-5):
-		j = i + 5
-		k = i + 2
-		if pots[i:j] in observations.keys():
-			next_gen[k] = observations[pots[i:j]]
-	pots = ''.join(next_gen)
-
-print(plants)
-
-"""
-#    #  ###  ### ########
-#   #   #  ##  # #
-#  #    #  ##  # #
-###     #  ##  # ########
-#  #    #      #        #
-#   #   #      #        #
-#    #  #      #        #
-#     # #      # ########
-"""
+for i in range(generations):
+	if i == 20:
+		print(f"There are {countPlants(c_gen)} after 20 generations")
+	c_gen = "."*4 + c_gen + "."*4
+	n_gen = ''
+	for n in range(2, len(c_gen) - 2):
+		state = c_gen[n-2:n+3]
+		n_gen += observations[state]
+	c_gen = n_gen
+	c_count = countPlants(c_gen)
+	differences.append(c_count - p_count)
+	p_count = c_count
+	if len(differences) > 100:
+			differences.pop(0)
+ld = sum(differences) // len(differences)
+total = (gg - generations) * ld + countPlants(c_gen)
+print(f"After {gg} generations there will be {total} plants.")
