db8ebea0c4e80c4b9c0e5ad425e44d8a88f66e3c
Ian S Pringle <ipringle@protonmail.com>
2019-03-19 20:14:57 -0400

    lintin'

---
+++ math/euler-proj/python/p23.py
@@ -0,0 +1,15 @@
+from lib.math import divisors
+
+goal = 28123
+sums = 0
+abundants = [i for i in range(goal) if sum(divisors(i)) > 2 * i]
+
+print(abundants)
+for n in [i for i in range(goal - 12) if i % 2 == 0 and i % 3 == 0]:
+	Z
+	if sum(divisors(n)) == n:
+		pass
+	else:
+		sums += n
+print(n)
+
