8d14b28f91a157e8d54342f13a2349e5125fafbf
Ian S. Pringle <ipringle2@liberty.edu>
2020-06-10 15:08:19 -0400

    Updated p1.py

--- math/euler-proj/python/p1.py
+++ math/euler-proj/python/p1.py
@@ -4,12 +4,4 @@
 # Find the sum of all the multiples of 3 or 5 below 1000.
 # Answer: 233168
 
-multiples = []
-i = 3
-
-while i < 1000:
-    if i % 3 == 0 or i % 5 == 0:
-        multiples.append(i)
-    i += 1
-
-print(sum(multiples))
+print(sum([x for x in range(1, 1000) if x % 3 == 0 or x % 5 == 0]))
