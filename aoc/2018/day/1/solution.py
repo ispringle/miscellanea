7264b35685a07ef6478d3a3622e450e38e0f1c2e
Ian S Pringle <ipringle@oteemo.com>
2021-12-02 17:37:27 -0600

    cleaning

--- aoc/2018/day/1/solution.py
+++ aoc/2018/day/1/solution.py
@@ -1,37 +1,35 @@
-from input import input
-import time
+def partOne(puzzle):
+    return sum(puzzle)
 
-def partOne():
-	return sum(input)
+def partTwo(puzzle):
+    t_input = tuple(puzzle)
+    prev = set([0])
+    curr = list(t_input)
+    summ = 0
 
-def partTwoBruteForce():
-	t_input = tuple(input)
-	prev = set([0])
-	curr = list(t_input)
-	sum = 0
-	
-	while True:
-		try:
-			sum += curr.pop(0)
-		except:
-			curr = list(t_input)
-			sum += curr.pop(0)
-	
-		if sum in prev:
-			return sum
-		else:
-			prev.add(sum)
+    while True:
+        try:
+            summ += curr.pop(0)
+        except Exception:
+            curr = list(t_input)
+            summ += curr.pop(0)
 
-s_one = partOne()
-if s_one == 0:
-	s_two = 0
-else:
-	start = time.time()
-	s_two = partTwoBruteForce()
-	end = time.time()
-	total = end - start
+        if summ in prev:
+            return summ
+        else:
+            prev.add(summ)
 
-print("The frequency of the initial pass is " + str(s_one) 
-		+ " and the first repeat frequency is " + str(s_two)
-		+ ".\nIt took " + str(total)
-		+ " seconds to run part two.")
+def clean(puzzle):
+    return [int(x) for x in puzzle.split('\n')]
+
+def solve(puzzle):
+    puzzle = clean(puzzle)
+    a = partOne(puzzle)
+    if a == 0:
+        b = 0
+    else:
+        b = partTwo(puzzle)
+    return (a, b)
+
+if __name__ == "__main__":
+    solve(puzzle)
