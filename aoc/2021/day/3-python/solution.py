8411d23f5295a41ea5e453e09d7a597a94fa125e
Ian S Pringle <ipringle@oteemo.com>
2021-12-03 15:39:34 -0600

    A sick python 1-liner for part 1, just for fun :D

---
+++ aoc/2021/day/3-python/solution.py
@@ -0,0 +1,6 @@
+i = [list(map(int, x.strip())) for x in open('test.txt').readlines()]
+print(
+    (ba_d := lambda x: int(''.join(str(y) for y in x), 2))(
+        gb := [int(x >= len(i)-x) for x in list(
+            map(sum, list(map(list, zip(*i)))))]
+    ) * ba_d([int(not x) for x in gb]))
