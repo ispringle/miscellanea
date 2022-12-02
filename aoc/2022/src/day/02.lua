85e6eb9899451a152d810790be88ef71e2768b7e
Ian Pringle <ian@hydrobuilder.com>
2022-12-02 06:13:53 -0600

    made it smol

--- aoc/2022/src/day/02.lua
+++ aoc/2022/src/day/02.lua
@@ -21,42 +21,18 @@ end
 
 local function part_one(input)
     local scores = {
-        A = {
-            X = 4,
-            Y = 8,
-            Z = 3,
-        },
-        B = {
-            X = 1,
-            Y = 5,
-            Z = 9,
-        },
-        C = {
-            X = 7,
-            Y = 2,
-            Z = 6,
-        },
+        A = { X = 4, Y = 8, Z = 3, },
+        B = { X = 1, Y = 5, Z = 9, },
+        C = { X = 7, Y = 2, Z = 6, },
     }
     return calculate_score(scores, input)
 end
 
 local function part_two(input)
     local scores = {
-        A = {
-            X = 3,
-            Y = 4,
-            Z = 8,
-        },
-        B = {
-            X = 1,
-            Y = 5,
-            Z = 9,
-        },
-        C = {
-            X = 2,
-            Y = 6,
-            Z = 7,
-        },
+        A = { X = 3, Y = 4, Z = 8, },
+        B = { X = 1, Y = 5, Z = 9, },
+        C = { X = 2, Y = 6, Z = 7, },
     }
     return calculate_score(scores, input)
 end
