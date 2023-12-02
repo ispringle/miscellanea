09022304f2bb70531f016fca8dde218c291337f8
Ian S. Pringle <ipringle@jackhenry.com>
2023-12-02 14:04:47 -0600

    cleanup

--- aoc/2023/src/day/two/solution.js
+++ aoc/2023/src/day/two/solution.js
@@ -1,5 +1,5 @@
-const parse = (input) => {
-  return input.split('\n').map((game) => ({
+const parse = (input) =>
+  input.split('\n').map((game) => ({
     red: Math.max(
       ...game.match(/\d+ red/g).map((s) => Number(s.match(/\d+/)[0])),
     ),
@@ -10,36 +10,31 @@ const parse = (input) => {
       ...game.match(/\d+ green/g).map((s) => Number(s.match(/\d+/)[0])),
     ),
   }));
-};
 
-const partOne = (input) => {
+const partOne = (input, rules) =>
+  input
+    .map((max, i) =>
+      Object.entries(max).every(([c, n]) => rules[c] >= n) ? i + 1 : 0,
+    )
+    .reduce((acc, n) => acc + n, 0);
+
+const partTwo = (input) =>
+  input
+    .map((max) => Object.values(max).reduce((acc, x) => x * acc, 1))
+    .reduce((acc, n) => acc + n, 0);
+
+/**
+ * @param {string} inputOne
+ * @param {string} inputTwo
+ * @returns [number, number]
+ */
+export default function solver(input) {
   const rules = {
     red: 12,
     green: 13,
     blue: 14,
   };
 
-  return input
-    .map((max, i) =>
-      Object.entries(max).every(([color, count]) => rules[color] >= count)
-        ? i + 1
-        : 0,
-    )
-    .reduce((acc, n) => acc + n, 0);
-};
-
-const partTwo = (input) => {
-  return input
-    .map((max) => Object.values(max).reduce((acc, x) => x * acc, 1))
-    .reduce((acc, n) => acc + n, 0);
-};
-
-/**
- * @param {string} inputOne
- * @param {string} inputTwo
- * @returns [any, any]
- */
-export default function solver(input) {
   const parsed = parse(input);
-  return [partOne(parsed), partTwo(parsed)];
+  return [partOne(parsed, rules), partTwo(parsed)];
 }
