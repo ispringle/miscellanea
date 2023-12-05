6c65747d018a6afb2f5e34c6197d3eca7c259693
Ian S. Pringle <ipringle@jackhenry.com>
2023-12-05 14:53:53 -0600

    minimized import/export code

--- aoc/2023/src/day/index.js
+++ aoc/2023/src/day/index.js
@@ -1,46 +1,13 @@
-import {
-  puzzleInput as dayOnePuzzleInput,
-  solver as dayOneSolver,
-  testInputs as dayOneTestInputs,
-} from './one';
-
-import {
-  solver as dayTwoSolver,
-  testInputs as dayTwoTestInputs,
-  puzzleInput as dayTwoPuzzleInput,
-} from './two';
-
-import {
-  solver as dayThreeSolver,
-  testInputs as dayThreeTestInputs,
-  puzzleInput as dayThreePuzzleInput,
-} from './three';
-
-import {
-  solver as dayFourSolver,
-  testInputs as dayFourTestInputs,
-  puzzleInput as dayFourPuzzleInput,
-} from './four';
+import one from './one';
+import two from './two';
+import three from './three';
+import four from './four';
+import five from './five';
 
 export default {
-  one: {
-    puzzleInput: dayOnePuzzleInput,
-    solve: dayOneSolver,
-    testInputs: dayOneTestInputs,
-  },
-  two: {
-    testInputs: dayTwoTestInputs,
-    puzzleInput: dayTwoPuzzleInput,
-    solve: dayTwoSolver,
-  },
-  three: {
-    testInputs: dayThreeTestInputs,
-    puzzleInput: dayThreePuzzleInput,
-    solve: dayThreeSolver,
-  },
-  four: {
-    testInputs: dayFourTestInputs,
-    puzzleInput: dayFourPuzzleInput,
-    solve: dayFourSolver,
-  },
+  one,
+  two,
+  three,
+  four,
+  five,
 };
--- aoc/2023/src/day/three/index.js
+++ aoc/2023/src/day/three/index.js
@@ -2,4 +2,8 @@ import solver from './solution.js';
 import puzzleInput from './puzzle-input.js';
 import testInputs from './test-inputs.js';
 
-export { solver, testInputs, puzzleInput };
+export default {
+  testInputs: testInputs,
+  puzzleInput: puzzleInput,
+  solve: solver,
+};
