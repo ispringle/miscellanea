b20f722c6c5e4a0ee6296aa9caa67011e26cd046
Ian S. Pringle <ipringle@jackhenry.com>
2023-12-06 08:47:11 -0600

    an efficient 5.2, but it's ugly

--- aoc/2023/src/day/five/solution.js
+++ aoc/2023/src/day/five/solution.js
@@ -7,8 +7,10 @@ import { chunk, range } from '../utils/index.js';
  * @returns number
  */
 const destination = (start, map) => {
-  return map.reduce((found, [source, max, dest]) => {
-    return source <= start && start < max ? dest + (start - source) : found;
+  return map.reduce((found, [dest, source, offset]) => {
+    return source <= start && start < source + offset
+      ? dest + (start - source)
+      : found;
   }, start);
 };
 
@@ -19,16 +21,44 @@ const partOne = ([seeds, seedMaps]) =>
     ),
   );
 
-const partTwo = ([seeds, seedMaps]) =>
-  Math.min(
-    ...chunk(seeds, 2).map(([start, length]) =>
-      Math.min(
-        ...range(length, start).map((seed) =>
-          seedMaps.reduce((next, map) => destination(next, map), seed),
-        ),
-      ),
-    ),
-  );
+const partTwo = ([seeds, seedMaps]) => {
+  const locations = [];
+  chunk(seeds, 2).forEach(([begin, len]) => {
+    let ranges = Array([begin, begin + len]);
+    let res = [];
+    seedMaps.forEach((map) => {
+      while (ranges.length !== 0) {
+        let [startRange, endRange] = ranges.pop();
+        let found = false;
+        for (const [target, startMap, r] of map) {
+          const endMap = startMap + r;
+          const offset = target - startMap;
+          if (endMap <= startRange || endRange <= startMap) {
+            continue;
+          }
+          if (startRange < startMap) {
+            ranges.push([startRange, startMap]);
+            startRange = startMap;
+          }
+          if (endMap < endRange) {
+            ranges.push([endMap, endRange]);
+            endRange = endMap;
+          }
+          res.push([startRange + offset, endRange + offset]);
+          found = true;
+          break;
+        }
+        if (!found) {
+          res.push([startRange, endRange]);
+        }
+      }
+      ranges = res;
+      res = [];
+    });
+    locations.push(...ranges.map(range => range[0]));
+  });
+  return Math.min(...locations)
+};
 
 const parse = (input) => {
   const [s, ...pages] = input.split('\n\n');
@@ -37,12 +67,7 @@ const parse = (input) => {
   pages.forEach((page) => {
     // eslint-disable-next-line no-unused-vars
     const [mapHeader, ...rules] = page.split('\n');
-    maps.push(
-      rules.map((rule) => {
-        const [dest, source, offset] = rule.split(' ').map(Number);
-        return [source, source + offset, dest];
-      }),
-    );
+    maps.push(rules.map((rule) => rule.split(' ').map(Number)));
   });
   return [seeds, maps];
 };
