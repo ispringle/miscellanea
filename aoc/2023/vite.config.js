0019444f5f9422ec5f54e46302298815693ddc14
Ian S. Pringle <ipringle@jackhenry.com>
2023-12-08 09:35:39 -0600

    only use copy plugin in prod

--- aoc/2023/vite.config.js
+++ aoc/2023/vite.config.js
@@ -1,15 +1,19 @@
 import { defineConfig } from 'vite';
-import {viteStaticCopy} from 'vite-plugin-static-copy';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-export default defineConfig({
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-        src: 'src/day/',
-        dest: './src/'
-        }
-      ]
-    })
-  ]
+export default defineConfig((configEnv) => {
+  return {
+    plugins: [
+      configEnv.mode === 'development'
+        ? undefined
+        : viteStaticCopy({
+            targets: [
+              {
+                src: 'src/day/',
+                dest: './src/',
+              },
+            ],
+          }),
+    ],
+  };
 });
