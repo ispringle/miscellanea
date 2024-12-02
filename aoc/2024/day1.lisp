768f45a12484807015be2d475369e9e0c64a83dc
Ian S. Pringle <ian@dapringles.com>
2024-12-02 14:36:08 -0600

    day 1 of AOC in the bag

---
+++ aoc/2024/day1.lisp
@@ -0,0 +1,25 @@
+(ql:quickload "str")
+
+(defvar test-input
+  "3   4
+4   3
+2   5
+1   3
+3   9
+3   3")
+
+(defun solve (input)
+  (let* ((lists (apply #'mapcar #'(lambda (&rest ns)
+                                   (mapcar #'parse-integer ns))
+                      (loop for line in (str:lines input)
+                            collect (str:words line))))
+        ;; Solves for part two
+        (s2 (reduce #'+ (mapcar #'(lambda (n) (* n (count n (cadr lists)))) (car lists))))
+        ;; Solves for part one
+        (s1 (reduce #'+ (mapcar #'(lambda (left right) (abs (- left right)))
+                                (sort (car lists) #'<)
+                                (sort (cadr lists) #'<)))))
+    (list s1 s2)))
+
+(solve (uiop:read-file-string "day1.txt"))
+(solve test-input)                      ; => (11 31)
